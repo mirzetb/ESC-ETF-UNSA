a4c8cc2fd84e9ce56fe630265dbeeff8a9a40486
ncongo <ncongo1@etf.unsa.ba>
2017-03-19 21:32:31 +0100

    Created project

---
+++ esc-users/src/main/java/com/project/nwt/EscUsersApplication.java
@@ -0,0 +1,12 @@
+package com.project.nwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscUsersApplication.class, args);
+	}
+}
