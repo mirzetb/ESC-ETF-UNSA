6c5977d68d7a0fb168bf716bda77fb499a58f545
encore13 <alisovac1@etf.unsa.ba>
2017-04-04 21:15:56 +0200

    RestController eureka esc-users

--- esc-users/src/main/java/com/project/nwt/EscUsersApplication.java
+++ esc-users/src/main/java/com/project/nwt/EscUsersApplication.java
@@ -4,19 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +38,20 @@ public class EscUsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscUsersApplication.class, args);
+	}
+	
+	
+	@RestController
+	class ServiceInstanceRestController {
+
+	    @Autowired
+	    private DiscoveryClient discoveryClient;
+
+	    @RequestMapping("/service-instances/{applicationName}")
+	    public List<ServiceInstance> serviceInstancesByApplicationName(
+	            @PathVariable String applicationName) {
+	        return this.discoveryClient.getInstances(applicationName);
+	    }
 	}
 	
 	@Bean
